3d67a28ade2d898697b3170232a5ac375c4d801f
Wesley <wesleyromantaylor1987@gmail.com>
2026-03-22 18:04:06 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Ticketing App</div>} />
+    </Routes>
+  );
+}
+
+export default App;
